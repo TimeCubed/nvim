18514e90dda104af3d1de39b96acae727e01f031
TimeSquared <48620951+TimeCubed@users.noreply.github.com>
2025-07-30 06:15:00 +0300

    set theme to gruv-vsassist due to some issues

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@
 require('lualine').setup {
 	options = {
 		icons_enabled = true,
-		theme = 'auto',
+		theme = 'gruv-vsassist',
 		component_separators = { left = '', right = ''},
 		section_separators = { left = '', right = ''},
 		disabled_filetypes = {
