a45662f322a37e3f7141148c9669a9bc35311abc
TimeSquared <48620951+TimeCubed@users.noreply.github.com>
2025-07-30 06:15:43 +0300

    fixed nvim-cmp setup

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -14,12 +14,14 @@ cmp.setup({
 			luasnip.lsp_expand(args.body)
 		end
 	},
-	sources = {
+	sources = cmp.config.sources({
+		{name = 'nvim_lsp'},
 		{name = 'nvim_lsp_signature_help'},
 		{name = 'path'},
 		{name = 'luasnip'},
+	}, {
 		{name = 'buffer'},
-	},
+	}),
 	window = {
 		completion = cmp.config.window.bordered(),
 		--documentation = cmp.config.window.bordered(),
