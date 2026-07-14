bd755334870494b29ad5263fb641e69c936d3103
tibbytabby <tibbytabby@noreply.codeberg.org>
2026-07-14 06:24:56 +0300

    lualine: switched theme to 'auto'

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@
 require('lualine').setup {
 	options = {
 		icons_enabled = true,
-		theme = 'gruv-vsassist',
+		theme = 'auto',
 		component_separators = { left = '', right = ''},
 		section_separators = { left = '', right = ''},
 		disabled_filetypes = {
