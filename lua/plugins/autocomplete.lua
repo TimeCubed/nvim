03d8448aeba3cd50bd372b631d7218764f407086
TimeCubed <48620951+TimeCubed@users.noreply.github.com>
2026-07-20 21:30:41 +0300

    cmp: updated config

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,72 +1,81 @@
 -- nvim-cmp config.
+local registry = require('core.plugins.registry')
 
-vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
-
-local cmp = require('cmp')
-local luasnip = require('luasnip')
-
-local select_opts = {behavior = cmp.SelectBehavior.Select}
-
-vim.o.winborder = "rounded"
-
-cmp.setup({
-	-- ohh it sets it up *here*. ok past me. you won there.
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end
+registry.register({
+	spec = {
+		'hrsh7th/nvim-cmp',
+		lazy = false
 	},
-	sources = cmp.config.sources({
-		{name = 'nvim_lsp'},
-		{name = 'nvim_lsp_signature_help'},
-		{name = 'path'},
-		{name = 'luasnip'},
-	}, {
-		{name = 'buffer'},
-	}),
-	window = {
-		completion = cmp.config.window.bordered({}),
-		documentation = cmp.config.window.bordered({}),
-	},
-	mapping = {
-		['<Up>'] = cmp.mapping.select_prev_item(select_opts),
-		['<Down>'] = cmp.mapping.select_next_item(select_opts),
+	setup = function()
+		vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 
-		['<C-u>'] = cmp.mapping.scroll_docs(-4),
-		['<C-d>'] = cmp.mapping.scroll_docs(4),
+		local cmp = require('cmp')
+		local luasnip = require('luasnip')
 
-		['<C-e>'] = cmp.mapping.abort(),
+		local select_opts = {behavior = cmp.SelectBehavior.Select}
 
-		['<C-y>'] = cmp.mapping.confirm({select = true}),
-		['<Tab>'] = cmp.mapping.confirm({select = true}),
+		vim.o.winborder = "rounded"
 
-		-- disable <CR> as the confirm bind
-		['<CR>'] = cmp.mapping.confirm({select = false}),
+		cmp.setup({
+			-- ohh it sets it up *here*. ok past me. you won there.
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end
+			},
+			sources = cmp.config.sources({
+				{name = 'nvim_lsp'},
+				{name = 'nvim_lsp_signature_help'},
+				{name = 'path'},
+				{name = 'luasnip'},
+			}, {
+				{name = 'buffer'},
+			}),
+			window = {
+				completion = cmp.config.window.bordered({}),
+				documentation = cmp.config.window.bordered({}),
+			},
+			mapping = {
+				['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+				['<Down>'] = cmp.mapping.select_next_item(select_opts),
 
-		['<C-f>'] = cmp.mapping(function(fallback)
-			if luasnip.jumpable(1) then
-				luasnip.jump(1)
-			end
-			--else
-			--	fallback()
-			--end
-		end, {'i', 's'}),
+				['<C-u>'] = cmp.mapping.scroll_docs(-4),
+				['<C-d>'] = cmp.mapping.scroll_docs(4),
 
-		['<C-b>'] = cmp.mapping(function(fallback)
-			if luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			end
-			--else
-			--	fallback()
-			--end
-		end, {'i', 's'}),
+				['<C-e>'] = cmp.mapping.abort(),
 
-		['<S-Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item(select_opts)
-			else
-				fallback()
-			end
-		end, {'i', 's'}),
-	},
+				['<C-y>'] = cmp.mapping.confirm({select = true}),
+				['<Tab>'] = cmp.mapping.confirm({select = true}),
+
+				-- disable <CR> as the confirm bind
+				['<CR>'] = cmp.mapping.confirm({select = false}),
+
+				['<C-f>'] = cmp.mapping(function(fallback)
+					if luasnip.jumpable(1) then
+						luasnip.jump(1)
+					end
+					--else
+					--	fallback()
+					--end
+				end, {'i', 's'}),
+
+				['<C-b>'] = cmp.mapping(function(fallback)
+					if luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					end
+					--else
+					--	fallback()
+					--end
+				end, {'i', 's'}),
+
+				['<S-Tab>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item(select_opts)
+					else
+						fallback()
+					end
+				end, {'i', 's'}),
+			},
+		})
+	end
 })
