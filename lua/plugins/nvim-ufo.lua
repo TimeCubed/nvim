45b4a2f1fdcd25811e58dbdd92130338d53d03c2
TimeCubed <48620951+TimeCubed@users.noreply.github.com>
2026-07-20 21:27:33 +0300

    nvim-ufo: updated config

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,5 +1,15 @@
-require('ufo').setup({
-	provider_selector = function(bufnr, filetype, buftype)
-		return { 'lsp', 'indent' }
+local registry = require('core.plugins.registry')
+
+registry.register({
+	spec = {
+		'kevinhwang91/nvim-ufo',
+		dependencies = 'kevinhwang91/promise-async'
+	},
+	setup = function()
+		require('ufo').setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { 'lsp', 'indent' }
+			end
+		})
 	end
 })
