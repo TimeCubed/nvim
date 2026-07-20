da72421e1d6699db2974e8bb869b89e2c0430289
TimeCubed <48620951+TimeCubed@users.noreply.github.com>
2026-07-20 21:27:15 +0300

    winresize: updated config

--- lua/plugins/winresize.lua
+++ lua/plugins/winresize.lua
@@ -1,17 +1,24 @@
-local resize = function(win, amt, dir)
-	return function()
-		require("winresize").resize(win, amt, dir)
+local registry = require('core.plugins.registry')
+
+registry.register({
+	spec = {'pogyomo/winresize.nvim'},
+	setup = function()
+		local resize = function(win, amt, dir)
+			return function()
+				require("winresize").resize(win, amt, dir)
+			end
+		end
+
+		-- resize window in different directions
+		vim.keymap.set('n', '<C-Up>',    resize(0, 2, "up")   )
+		vim.keymap.set('n', '<C-Down>',  resize(0, 2, "down") )
+		vim.keymap.set('n', '<C-Left>',  resize(0, 2, "left") )
+		vim.keymap.set('n', '<C-Right>', resize(0, 2, "right"))
+
+		-- resize but for terminal mode as well
+		vim.keymap.set('t', '<C-Up>',    resize(0, 2, "up")   )
+		vim.keymap.set('t', '<C-Down>',  resize(0, 2, "down") )
+		vim.keymap.set('t', '<C-Left>',  resize(0, 2, "left") )
+		vim.keymap.set('t', '<C-Right>', resize(0, 2, "right"))
 	end
-end
-
--- resize window in different directions
-vim.keymap.set('n', '<C-Up>',    resize(0, 2, "up")   )
-vim.keymap.set('n', '<C-Down>',  resize(0, 2, "down") )
-vim.keymap.set('n', '<C-Left>',  resize(0, 2, "left") )
-vim.keymap.set('n', '<C-Right>', resize(0, 2, "right"))
-
--- resize but for terminal mode as well
-vim.keymap.set('t', '<C-Up>',    resize(0, 2, "up")   )
-vim.keymap.set('t', '<C-Down>',  resize(0, 2, "down") )
-vim.keymap.set('t', '<C-Left>',  resize(0, 2, "left") )
-vim.keymap.set('t', '<C-Right>', resize(0, 2, "right"))
+})
