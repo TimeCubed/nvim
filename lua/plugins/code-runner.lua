284eeaceb04620e0721800606ffebec2f99c2864
TimeCubed <48620951+TimeCubed@users.noreply.github.com>
2026-07-20 21:30:28 +0300

    code-runner: updated config

--- lua/plugins/code-runner.lua
+++ lua/plugins/code-runner.lua
@@ -1,34 +1,40 @@
 -- code_runner config.
 -- runs a small shell script when :RunCode is executed, depending on the filetype.
+local registry = require('core.plugins.registry')
 
-require('code_runner').setup({
-	filetype = {
-		cpp = {
-			"cd \"$dir\"; ",
-			"gcc $fileName -o main -lstdc++ -lm && ",
-			"echo \"Compilation complete!\" && ",
-			"./main &&",
-			"rm main"
-		},
+registry.register({
+	spec = {'CRAG666/code_runner.nvim'},
+	setup = function()
+		require('code_runner').setup({
+			filetype = {
+				cpp = {
+					"cd \"$dir\"; ",
+					"gcc $fileName -o main -lstdc++ -lm && ",
+					"echo \"Compilation complete!\" && ",
+					"./main &&",
+					"rm main"
+				},
 
-		c = {
-			"cd \"$dir\" && ",
-			"gcc $fileName -o /tmp/$fileNameWithoutExt && ",
-			"/tmp/$fileNameWithoutExt && ",
-			"rm /tmp/$fileNameWithoutExt",
-		},
+				c = {
+					"cd \"$dir\" && ",
+					"gcc $fileName -o /tmp/$fileNameWithoutExt && ",
+					"/tmp/$fileNameWithoutExt && ",
+					"rm /tmp/$fileNameWithoutExt",
+				},
 
-		rust = {
-			"cd $dir/../ && ",
-			"cargo run"
-		},
+				rust = {
+					"cd $dir/../ && ",
+					"cargo run"
+				},
 
-		python = {
-			"cd $dir && ",
-			"python3 $fileName"
-		}
-	}
+				python = {
+					"cd $dir && ",
+					"python3 $fileName"
+				}
+			}
+		})
+
+		-- quick :RunCode keymap.
+		vim.keymap.set('n', '<leader>ru', '<cmd>RunCode<cr>i')
+	end
 })
-
--- quick :RunCode keymap.
-vim.keymap.set('n', '<leader>ru', '<cmd>RunCode<cr>i')
