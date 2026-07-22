ebb2a36ce5e6d824f4784e13799b3e8426ade611
TimeCubed <48620951+TimeCubed@users.noreply.github.com>
2026-07-22 21:31:19 +0300

    telescope: added keymaps to the setup function

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,4 +2,11 @@ local registry = require('core.plugins.registry')
 
 registry.register({
 	spec = {'nvim-telescope/telescope.nvim'},
+	setup = function()
+		-- telescope keymaps.
+		vim.keymap.set('n', '<leader>ff', require("telescope.builtin").find_files)
+		vim.keymap.set('n', '<leader>fg', require("telescope.builtin").live_grep)
+		vim.keymap.set('n', '<leader>fb', require("telescope.builtin").buffers)
+		vim.keymap.set('n', '<leader>fh', require("telescope.builtin").help_tags)
+	end
 })
