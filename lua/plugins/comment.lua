c3d5d023ea3e394062073c2e9d06a7b5e13cdcdd
TimeCubed <48620951+TimeCubed@users.noreply.github.com>
2026-08-04 23:42:56 +0300

    comment: changed comment keymaps

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -3,6 +3,15 @@ local registry = require('core.plugins.registry')
 registry.register({
 	spec = {'numToStr/Comment.nvim'},
 	setup = function()
-		require('Comment').setup()
+		require('Comment').setup({
+			toggler = {
+				line = '<leader>lc',
+				block = '<leader>bc'
+			},
+			opleader = {
+				line = 'nc',
+				block = 'bc'
+			}
+		})
 	end
 })
